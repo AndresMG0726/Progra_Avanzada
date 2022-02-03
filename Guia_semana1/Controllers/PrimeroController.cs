7d9f6ba10af231da6b7e32e1877983ffc5962981
AndresMG0726 <andresmora.g10@gmail.com>
2022-02-03 00:25:56 -0600

    guia#1
    la aplicación tiene los 2 controladores y las 4 vistas, no sabia que era lo que había que hacer en si, por eso hice la lista de pares-impares en JavaScript, las páginas 2,3 están vacías(pero funcionan ) y la 4 tiene un video de relleno

---
+++ Guia_semana1/Controllers/PrimeroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Guia_semana1.Controllers
+{
+    public class PrimeroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int primero, int segundo)
+        {
+
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
